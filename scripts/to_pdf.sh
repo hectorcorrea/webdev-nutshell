da7a8af239c9b424d6193d2a2e369cd7a794403d
Hector Correa <hector_correa@princeton.edu>
2023-12-19 17:31:39 -0500

    Scripts to produce a PDF file with the tutorial

---
+++ scripts/to_pdf.sh
@@ -0,0 +1,31 @@
+# Creates a PDF version of tutorial.md
+# via pandoc and wkhtmltopdf.
+
+# Convert the markdown file to HTML
+# https://pandoc.org/MANUAL.html
+#
+pandoc tutorial.md \
+-f markdown \
+-t html -s -o tutorial.html \
+-c scripts/pandoc_styles.css \
+--toc -V toc-title:"Table of Contents" \
+--metadata pagetitle="Web development in a nutshell workshop"
+
+# Convert the HTML file to PDF
+# https://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+# (use the installer from wkhtmltopdf.org, not from Homebrew)
+#
+wkhtmltopdf \
+--footer-line \
+--footer-left "Web development in a nutshell workshop" \
+--footer-right "[page]/[toPage]" \
+--footer-spacing 20 \
+--margin-top 15 \
+--margin-left 15 \
+--margin-bottom 30 \
+--margin-right 15 \
+--dpi 120 \
+--enable-local-file-access \
+tutorial.html tutorial.pdf
+
+rm tutorial.html
